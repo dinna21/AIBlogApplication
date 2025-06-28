a7974eba1098df3cf7f5f8d4d0b5a004ee4e5238
dinna21 <ransikadinura@gmail.com>
2025-06-28 18:01:03 +0530

    created some functionalites using foms and introduction

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,8 +9,17 @@ export default function Header() {
                 <p>New AI Feature Intergrated </p>
                 <img src={assets.star_icon} className='w-2.5' alt="" />
             </div>
+            <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700'>Your own <span className='text-primary'>blogging</span><br />Platform </h1>
+            <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'>
+                Create, share and monetize your content with our AI-powered blogging platform.This is your space to express yourself, connect with others, and build a community around your ideas.
+            </p>
+            <form action="" className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 rounded-full px-4 py-2.5 bg-white shadow-md'>
+                <input className='w-full pl-4 outline-none'  type="text" name='' placeholder='Search For blogs' required />
+                <button className='bg-primary text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer' type='submit'>Search</button>
+            </form>
         </div>
         <img src={assets.gradientBackground} className='absolute -top-50 -z-1 opacity-50' alt="" />
+
     </div>
   )
 }
