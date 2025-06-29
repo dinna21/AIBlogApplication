b75328d77bdad811569decb66d5b7f34b6f8dd86
dinna21 <ransikadinura@gmail.com>
2025-06-29 17:23:46 +0530

    add som fucntionality for single blog page usng useEffect,useParams and other

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { blog_data } from '../assets/assets';
+import Navbar from '../components/Navbar';
 
 export default function Blog() {
-  return (
-    <div>Blog</div>
-  )
+  const { id } = useParams();
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchBlogData = async () => {
+      const found = blog_data.find(item => item._id === id);
+      setData(found);
+    };
+    fetchBlogData();
+  }, [id]); // depend on `id` so it runs when route changes
+
+  return data ? (
+    <div>
+      <Navbar />
+      <div>
+
+      </div>
+
+      <div>
+        
+      </div>
+    </div>
+  ) : (
+    <div>Loading…</div>
+  );
 }
