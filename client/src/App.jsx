cf93ff72b917a6b069b8967a99d83ff28f22e9ba
dinna21 <ransikadinura@gmail.com>
2025-07-02 19:32:46 +0530

    created the routes for the admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
+import Layout from './pages/admin/Layout'
+import Addblog from './pages/admin/Addblog'
+import ListBlog from './pages/admin/ListBlog'
+import Comments from './pages/admin/Comments'
+import Dashboard from './pages/admin/Dashboard'
+
 
 export default function App() {
   return (
@@ -9,8 +15,13 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={<Layout />}>
+          <Route path="addBlog" element={<Addblog />} />
+          <Route path="ListBlog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
+          <Route index element={<Dashboard />} />
+        </Route>
       </Routes>
     </div>
-    
   )
 }
