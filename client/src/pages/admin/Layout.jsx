6755c0835350b6ac5c2d35991c19a7278dad85de
dinna21 <ransikadinura@gmail.com>
2025-07-03 19:24:05 +0530

    add the sidebar to admin which which in this file

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,6 +1,7 @@
 import React, { use } from 'react'
 import { assets } from '../../assets/assets'
-import { useNavigate } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import Sidebar from '../../components/admin/Sidebar'
 
 export default function Layout() {
   const navigate = useNavigate()
@@ -9,12 +10,16 @@ export default function Layout() {
   }
   return (
     <>
-        <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
+        <div className='flex items-center justify-between py-2 h-[70px] px-4 sm:px-12 border-b border-gray-400'>
         <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={() => navigate('/') } />
         <  button onClick={logout} className='text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer'>Logout</button>
         </div>
 
-        
+        <div className='flex h-[calc(100vh-70px)]'>
+            <Sidebar />
+            {/* <Outlet /> */}
+            {/* add the child elements in the app.jsx file  */}
+        </div>
     </>
   )
 }
