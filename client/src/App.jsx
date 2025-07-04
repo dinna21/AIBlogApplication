10756cd48fc31bb63d4283226df4e5bb67ff1f41
dinna21 <ransikadinura@gmail.com>
2025-07-05 01:04:26 +0530

    Add dashboard compone page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,11 +16,11 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={false ? <Layout />: <Login />}>
+        <Route path="/admin" element={true ? <Layout />: <Login />}>
           <Route path="addBlog" element={<Addblog />} />
           <Route path="ListBlog" element={<ListBlog />} />
           <Route path="comments" element={<Comments />} />
-
+          <Route path="" element={<Dashboard />} />
         </Route>
       </Routes>
     </div>
