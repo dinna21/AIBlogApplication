b6a6b148150fdfc9517a09795ca0371de354714b
dinna21 <ransikadinura@gmail.com>
2025-07-05 01:05:14 +0530

    update the login page when the admin access

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+    const handleSubmit = async (e) => {
+    e.preventDefault()
+    }
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
   return (
-
         <div className='flex items-center justify-center h-screen '>
           <div className='w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg' >
           <div className='flex flex-col items-center justify-center'>
@@ -11,7 +15,23 @@ export default function Login() {
               <p className='font-light'>Enter Your Credentials
              to access the admin panel</p>
           </div>
-
+          <form onSubmit={handleSubmit} className='mt-6 w-full sm:max-w-md text-gray-600'>
+              <div>
+                <label htmlFor="">Email</label>
+                <input
+                onChange={e=> setEmail(e.target.value)} value={email}
+                type="email" className='border-b-2 border-gray-300 p-2 outline-none mb-6' required  placeholder='Give Your Email'/>
+              </div>
+              <div>
+                <label htmlFor="">Password</label>
+                <input
+                onChange={e=> setPassword(e.target.value)} value={password}
+                type="password" className='border-b-2 border-gray-300 p-2 outline-none mb-6' required  placeholder='Give Your Password'/>
+              </div>
+              <button type='submit' className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 trasition-all'>
+                  Login
+              </button>
+          </form>
           </div>
           </div>
         </div>
