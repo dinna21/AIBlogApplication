d4af00359f987896ec48ee98cbaf477b3a36793b
dinna21 <ransikadinura@gmail.com>
2025-07-07 21:33:20 +0530

    create a Addblog page for the admin

--- client/src/pages/admin/Addblog.jsx
+++ client/src/pages/admin/Addblog.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
 
 export default function Addblog() {
+  const [image,setImage] = useState(false)
+  const [title,setTitle] = useState('')
+  const [subTitle,setSubTitle] = useState('')
+  const [catogery,setCatogery] = useState('Startup')
+  const [isPublished,setIspublished] = useState(false)
   return (
-    <div>Addblog</div>
+    <form action="" className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+      <div className='bg-white w-full max-w-3xl p-4 md:p-10 shadow rounded '>
+        <p>Upload Thubnail</p>
+        <label htmlFor="image">
+          <img src={assets.upload_area}  alt="" className='mt-2 h-16 rounded cursor-pointer' />
+          <input type="file" id='image' hidden required />
+        </label>
+      </div>
+    </form>
   )
 }
