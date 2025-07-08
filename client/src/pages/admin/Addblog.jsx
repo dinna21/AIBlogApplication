3a8a8fb2d9ac77942de999ebb46c1d9e38517245
dinna21 <ransikadinura@gmail.com>
2025-07-08 08:11:55 +0530

    when u want to upload the image how to code like this 🥹

--- client/src/pages/admin/Addblog.jsx
+++ client/src/pages/admin/Addblog.jsx
@@ -15,8 +15,8 @@ export default function Addblog() {
       <div className='bg-white w-full max-w-3xl p-4 md:p-10 shadow rounded '>
         <p>Upload Thubnail</p>
         <label htmlFor="image">
-          <img src={assets.upload_area}  alt="" className='mt-2 h-16 rounded cursor-pointer' />
-          <input type="file" id='image' hidden required />
+          <img src={!image ? assets.upload_area : URL.createObjectURL(image) }  alt="" className='mt-2 h-16 rounded cursor-pointer' />
+          <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' hidden required />
         </label>
       </div>
     </form>
