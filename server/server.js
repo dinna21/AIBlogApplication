e13b2ed049e2f5957c8ae29cc332f3724ad1dd88
dinna21 <ransikadinura@gmail.com>
2025-07-08 19:26:26 +0530

    create the basic express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+
+const app = express();
+
+//Midleware 
+app.use(cors());
+app.use(express.json());
+//Routes
+app.get('/',(req,res) => res.send("API is Working"));
+const port = process.env.port ||  3000;
+app.listen(port,()=>{
+    console.log('API is working on port '+port);
+})
+
