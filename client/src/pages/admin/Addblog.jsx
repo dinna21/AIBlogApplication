df1539830539433fa49a9f897f3c3c6386d8c8a9
dinna21 <ransikadinura@gmail.com>
2025-07-08 09:14:49 +0530

    Add the rich text icons using quill npm package quillRef and editorRef I used for that

--- client/src/pages/admin/Addblog.jsx
+++ client/src/pages/admin/Addblog.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { assets } from '../../assets/assets'
-
+import Quill from 'quill'
 export default function Addblog() {
+  const editorRef = useRef(null)
+  const quillRef = useRef(null)
   const [image,setImage] = useState(false)
   const [title,setTitle] = useState('')
   const [subTitle,setSubTitle] = useState('')
@@ -10,8 +12,19 @@ export default function Addblog() {
   const onSubmitHandler = async (e) => {
     e.preventDefault();
   }
+  // for generating ai automated the blog discrioption 
+  const generateContent = async (e) => {
+
+  }
+  useEffect(()=> {
+      if(!quillRef.current && editorRef.current)
+      {
+        quillRef.current = new Quill(editorRef.current, {theme: 'snow'})
+      }
+  },[])
+
   return (
-    <form  onSubmit={onSubmitHandler} action="" className='flex-1 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
+    <form  onSubmit={onSubmitHandler} action="" className='flex-1 p-15 bg-blue-50/50 text-gray-600 h-full overflow-scroll'>
       <div className='bg-white w-full max-w-3xl p-4 md:p-10 shadow rounded '>
         <p>Upload Thubnail</p>
         <label htmlFor="image">
@@ -19,8 +32,18 @@ export default function Addblog() {
           <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' hidden required />
         </label>
         <p className='mt-4'>Blog Title</p>
-        <input type="text" placeholder='Type Here ' required className='w-full max-w-lg mt-2 p-2 border-gray-300 outline-none rounded' onChange={e=>setTitle(e.target.value)} value={title} />
+        <input type="text" placeholder='Type Here ' required className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded' onChange={e=>setTitle(e.target.value)} value={title} />
+        
+        <p className='mt-4'>Sub Title</p>
+        <input type="text" placeholder='Type Here ' required className='w-full max-w-lg mt-2 p-2 border border-gray-300 outline-none rounded' onChange={e=>setSubTitle(e.target.value)} value={subTitle} />
 
+        <p className='mt-4'>Blog Description</p>
+        <div className='max-w-lg h-74 pb-16 pt-2 relative '>
+              <div ref={editorRef}>
+              </div>
+              <button type='button' className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer' onClick={generateContent}>Generate With AI</button>
+        </div>
+        
       </div>
     </form>
   )
