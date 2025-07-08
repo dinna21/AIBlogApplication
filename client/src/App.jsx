91a625d3d4cfc1d4f8ea36baf3b0d0141da7fb86
dinna21 <ransikadinura@gmail.com>
2025-07-08 09:00:05 +0530

    Import the quill packaege css part

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import ListBlog from './pages/admin/ListBlog'
 import Comments from './pages/admin/Comments'
 import Dashboard from './pages/admin/Dashboard'
 import Login from './components/admin/Login'
+import 'quill/dist/quill.snow.css'
 
 
 export default function App() {
@@ -15,7 +16,7 @@ export default function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/blog/:id" element={<Blog />}/>
         <Route path="/admin" element={true ? <Layout />: <Login />}>
           <Route path="addBlog" element={<Addblog />} />
           <Route path="ListBlog" element={<ListBlog />} />
