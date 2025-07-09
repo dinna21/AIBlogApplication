1c86562481ddca5bf5dba021d948ff9f07e4193f
dinna21 <ransikadinura@gmail.com>
2025-07-09 21:39:36 +0530

    add the api in the server file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './configs/db.js';
+import adminRouter from './routes/adminRoutes.js';
 
 await connectDB();
 const app = express();
@@ -11,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 //Routes
 app.get('/',(req,res) => res.send("API is Working"));
+app.use('/api/admin',adminRouter);
+
+
+
 const port = process.env.port ||  3000;
 app.listen(port,()=>{
     console.log('API is working on port '+port);
