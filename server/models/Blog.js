5b961195be64fefe7de76cbfed3fb9a71268de63
dinna21 <ransikadinura@gmail.com>
2025-07-09 23:36:16 +0530

    Defines the structure of your data and how it’s stored and validated in MongoDB

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,1 +1,15 @@
 import mongoose  from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    subTitle: {type: String},
+    description: {type: String, required: true},
+    category: {type: String, required: true},
+    // in this we added image url that's why image type is string
+    image: {type: String, required: true},
+    isPublished: {type: Boolean, required: true}
+},{timestamps: true})
+
+const Blog = mongoose.model('blog',blogSchema)
+
+export default Blog;
