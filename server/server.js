36330e4785e7aa1a752b7d016af7d9030765d8da
dinna21 <ransikadinura@gmail.com>
2025-07-11 00:04:33 +0530

    add the blog router in the server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
 
 await connectDB();
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 //Routes
 app.get('/',(req,res) => res.send("API is Working"));
 app.use('/api/admin',adminRouter);
+app.use('/api/blog',blogRouter);
 
 
 
