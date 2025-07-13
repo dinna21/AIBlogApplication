7caa37dd93d05a091b3e1c68f3c6e59e1a8db279
dinna21 <ransikadinura@gmail.com>
2025-07-13 05:51:01 +0530

    add the comment

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { addBlog } from "../controllers/blogController.js";
-import upload from '../middleware/multer.js'
+import upload from '../middleware/multer.js';
 import auth from "../middleware/auth.js";
 
 const  blogRouter = express.Router();
 
-blogRouter.post("/add",upload.single('image'),auth, addBlog);
+// blogRouter.post("/add", auth, upload.single("image"), addBlog);
+// ✅ Route expects 'image' field in form-data
+blogRouter.post("/add", auth, upload.single("image"), addBlog);
 
 export default blogRouter;
 
