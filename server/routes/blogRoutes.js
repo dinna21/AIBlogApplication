738cf8d3901985f906109aea5ba5c545fccab24c
dinna21 <ransikadinura@gmail.com>
2025-07-15 23:35:13 +0530

    add the routes for delete, getall and get by id for blogs

--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, deleteBlogByID, getAllBlogs, getBlogByID, togglePublish } from "../controllers/blogController.js";
 import upload from "../middleware/multer.js";
 import auth from "../middleware/auth.js";
 
@@ -8,4 +8,9 @@ const blogRouter = express.Router();
 // Correct middleware order:
 blogRouter.post("/add", auth, upload.single("image"), addBlog);
 export default blogRouter;
+blogRouter.get('/all', getAllBlogs);
+blogRouter.get('/:blogID',getBlogByID);
+blogRouter.post('/delete',auth, deleteBlogByID);
+blogRouter.post('/toggle-publish',auth, togglePublish);
+
 
