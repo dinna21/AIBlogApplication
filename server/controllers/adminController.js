fc06bbe5807f595f52d41093c934d603bf7a1034
dinna21 <ransikadinura@gmail.com>
2025-07-19 22:13:32 +0530

    add the code for deleteComment by id and approved commnet by id in the admincontroller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -43,8 +43,30 @@ export const getDashboard = async (req,res)=>{
             blogs,comments,drafts, recentBlogs
         }
         res.json({success: true, dashBoardData
-        })
+        });
     } catch (error) {
         res.json({success: false, message: error.message})
     }
 }
+
+export const deleteCommentById = async (req, res) => {
+    try {
+        const {id} = req.body;
+        await Comment.findByIdAndDelete(id);
+        res.json({success: true, message: "Comment Deleted Successfully"});
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+export const approveCommentById = async (req, res) => {
+    try {
+        const {id} = req.body;
+        const comment = await Comment.findByIdAndUpdate(id,{ isApproved: true });
+        res.json({success: true, message: "Comment Approved Successfully"});
+    } catch (error) {
+        res.json({success: false, message: error.message})
+    }
+}
+
+
