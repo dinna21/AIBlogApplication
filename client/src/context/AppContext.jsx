fa01fccf7de4664f73bb373cd5baaa4868e4f996
dinna21 <ransikadinura@gmail.com>
2025-07-20 11:49:59 +0530

    add the functions to fetch all the blogs and fetches all blog posts from your backend server

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,8 @@ import {createContext,useContext} from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import toast from "react-hot-toast";
+import { useEffect } from "react";
 
 const AppContext = createContext();
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
@@ -11,9 +13,28 @@ export const AppProvider = ({ children }) =>{
     const [blogs, setBlogs] = useState([]);
     const [input, setInput] = useState("");
 
-    const value = {
+    const fetchBlogs = async () =>
+    {
+        try {
+            const response = await axios.get("/api/blog/all");
+            if (response.data.success) {
+                setBlogs(response.data.blogs);
+            } else {
+                toast.error(response.data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+    useEffect(() => {
+        fetchBlogs();
+    }, []);
+    
+    const value =
+    {
         axios,navigate,token,setToken,blogs,setBlogs,input,setInput
     }
+
     return (
         <AppContext.Provider value={{}}>
             {children}
