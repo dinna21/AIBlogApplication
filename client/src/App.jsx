69ce3c32d84db250dd399fe2a7d804fa94797127
dinna21 <ransikadinura@gmail.com>
2025-07-20 11:13:50 +0530

    Add toaster notificatio alter in the app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,11 +9,14 @@ import Comments from './pages/admin/Comments'
 import Dashboard from './pages/admin/Dashboard'
 import Login from './components/admin/Login'
 import 'quill/dist/quill.snow.css'
+import {Toaster} from 'react-hot-toast'
+
 
 
 export default function App() {
   return (
     <div>
+      <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />}/>
