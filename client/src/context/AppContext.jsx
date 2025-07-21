e3bc471884aa9d360e446f17e34105e6eca53740
dinna21 <ransikadinura@gmail.com>
2025-07-21 23:52:27 +0530

    when the admin login it remember token in that period

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -28,6 +28,12 @@ export const AppProvider = ({ children }) =>{
     }
     useEffect(() => {
         fetchBlogs();
+        const token = localStorage.getItem("token");
+        if (token) {
+            setToken(token);
+            axios.defaults.headers.common["Authorization"] = ` ${token}`;
+        }
+
     }, []);
     
     const value =
