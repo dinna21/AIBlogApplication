d498017d4b7b0699c78a316823fecb24a9d56e7e
dinna21 <ransikadinura@gmail.com>
2025-07-24 19:35:38 +0530

    I fixed some issue of my App file which gave me bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,17 +10,19 @@ import Dashboard from './pages/admin/Dashboard'
 import Login from './components/admin/Login'
 import 'quill/dist/quill.snow.css'
 import {Toaster} from 'react-hot-toast'
+import { useAppContext } from './context/AppContext'
 
 
 
 export default function App() {
+  const {token} = useAppContext();
   return (
     <div>
       <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />}/>
-        <Route path="/admin" element={true ? <Layout />: <Login />}>
+        <Route path="/admin" element={token ? <Layout /> : <Login />}>
           <Route path="addBlog" element={<Addblog />} />
           <Route path="ListBlog" element={<ListBlog />} />
           <Route path="comments" element={<Comments />} />
